91c84d627ca93ba7537408165b0ef0702f84d398
UriOriKay <kay@Uriorie.de>
2023-03-15 20:33:08 +0100

    frist question appears

---
+++ script.js
@@ -0,0 +1,53 @@
+let questions = [
+    {
+        "question": "Wer ist deutscher Rekordmeister im Volleyball der Frauen?",
+        "answer_1": "MTV Stuttgart",
+        "answer_2": "USC Münster",
+        "answer_3": "SSC Schwerin",
+        "answer_4": "Rote Raben Vilsbiburg",
+        "right_answer": 4,
+        "category": "Sport"
+    },
+
+    {
+        "question": "In welchsem Jahr war Deutschland zuletzt Handball Europameister?",
+        "answer_1": "2007",
+        "answer_2": "2016",
+        "answer_3": "1998",
+        "answer_4": "1996",
+        "right_answer": 4,
+        "category": "Sport"
+    },
+
+    {
+        "question": "In welchem Regelwerk gibt es ein Textilvergehen?",
+        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
+        "answer_2": "&lt;iframe&gt;",
+        "answer_3": "&lt;frame&gt;",
+        "answer_4": "&lt;frameset&gt",
+        "right_answer": 2,
+        "category": "Sport"
+    },   
+]
+
+let currentQuestion = 0;
+
+function init(){
+    docId('q-len').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+
+function docId(id) {
+    return document.getElementById(id);
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+    docId('questiontext').innerHTML = question['question'];
+    docId('answer_1').innerHTML = question['answer_1'];
+    docId('answer_2').innerHTML = question['answer_2'];
+    docId('answer_3').innerHTML = question['answer_3'];
+    docId('answer_4').innerHTML = question['answer_4'];
+}
